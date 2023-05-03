132691fff0b0e3d05fa7618b9457e9da8280eb4e
dumpuswumpus <88466474+DumpusWumpus@users.noreply.github.com>
2023-05-03 00:06:07 -0400

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+just a practicing repo for creating npm packages using esm + typescript
---
+++ lib/utils.ts
@@ -0,0 +1,6 @@
+/**
+ * Used to add 2 numbers :O
+ */
+export function add (num1: number, num2: number) {
+	return num1 + num2
+}
